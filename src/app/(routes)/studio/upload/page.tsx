d28880e587b397805ae1e1a88b2a1d5947241e1b
admin <harshkurulkar7@gmail.com>
2023-12-27 17:53:43 +0530

    fixed: Upload Image Bug

--- src/app/(routes)/studio/upload/page.tsx
+++ src/app/(routes)/studio/upload/page.tsx
@@ -6,15 +6,25 @@ import VideoPreview from "@/components/studio/upload/VideoPreview";
 import VideoUploadForm from "@/components/studio/upload/VideoUploadForm";
 import { UploadVideoModalContext } from "@/context/UploadVideoModalContext";
 import { useRouter } from "next/navigation";
-import { useContext, useEffect } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { useContext, useEffect, useMemo, useState } from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { v4 as uuid } from "uuid";
+import { toast } from "react-hot-toast";
+import axios from "axios";
 
 export default function UploadPage() {
+  const uploadVideoModal = useContext(UploadVideoModalContext);
+
+  useEffect(() => uploadVideoModal?.onOpen(), []);
+
   const router = useRouter();
 
-  const uploadVideoModal = useContext(UploadVideoModalContext);
-  useEffect(() => {
-    uploadVideoModal?.onOpen();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const videoId = useMemo(() => {
+    const buffer = Buffer.alloc(12);
+
+    return uuid({}, buffer).toString("hex");
   }, []);
 
   const {
@@ -32,6 +42,9 @@ export default function UploadPage() {
     },
   });
 
+  const thumbnailSrc: string = watch("thumbnailSrc");
+  const videoSrc: string = watch("videoSrc");
+
   const changeValue = (id: string, value: string) => {
     setValue(id, value, {
       shouldDirty: true,
@@ -40,18 +53,28 @@ export default function UploadPage() {
     });
   };
 
-  const onSubmit = () => {};
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+    axios
+      .post("/api/videos", data)
+      .then(() => {
+        toast.success("Video published successfully");
+        router.push("/studio");
+      })
+      .catch(() => toast.error("Could not publish video"))
+      .finally(() => setIsLoading(false));
+  };
 
   return (
     <>
-      {/* <UploadVideoModal
-        onUpload={(value) => {
-          changeValue("videoSrc", value);
-        }}
-      /> */}
+      {uploadVideoModal?.isOpen && (
+        <UploadVideoModal
+          onUpload={(value) => changeValue("videoSrc", value)}
+        />
+      )}
       <div className="flex flex-col px-8 pt-4">
         <div className="flex justify-between">
-          <h1 className="text-2xl">Video Details</h1>
+          <h1 className="text-2xl">Video details</h1>
           <span className="flex gap-4">
             <Button type="secondary" onClick={() => router.back()}>
               Cancel
@@ -62,8 +85,14 @@ export default function UploadPage() {
           </span>
         </div>
         <div className="mt-6 flex flex-col md:flex-row gap-6 md:gap-2">
-          <VideoUploadForm />
-          <VideoPreview />
+          <VideoUploadForm
+            register={register}
+            errors={errors}
+            changeValue={changeValue}
+            thumbnailSrc={thumbnailSrc}
+            isLoading={isLoading}
+          />
+          <VideoPreview videoSrc={videoSrc} videoId={videoId} />
         </div>
       </div>
     </>
--- src/components/shared/Modal/UploadVideoModal.tsx
+++ src/components/shared/Modal/UploadVideoModal.tsx
@@ -2,7 +2,7 @@
 
 import { UploadVideoModalContext } from "@/context/UploadVideoModalContext";
 import { useRouter } from "next/navigation";
-import React, { useContext, useState } from "react";
+import { useContext } from "react";
 import { MdClose, MdUpload } from "react-icons/md";
 import MediaUpload from "../MediaUpload";
 import IconButton from "../IconButton";
@@ -14,11 +14,14 @@ interface UploadVideoModalProps {
 
 const UploadVideoModal: React.FC<UploadVideoModalProps> = ({ onUpload }) => {
   const router = useRouter();
+
   const uploadVideoModal = useContext(UploadVideoModalContext);
+
   const handleUpload = (value: string) => {
     onUpload(value);
     uploadVideoModal?.onClose();
   };
+
   return (
     <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col justify-start bg-zinc-800 w-5/6 h-5/6 rounded-xl z-50">
       <div className="p-3 border-b border-neutral-700 flex justify-between">
@@ -32,26 +35,18 @@ const UploadVideoModal: React.FC<UploadVideoModalProps> = ({ onUpload }) => {
         />
       </div>
       <div className="flex flex-col gap-4 justify-center items-center h-full">
-        <MediaUpload
-          onChange={() => {
-            handleUpload;
-          }}
-        >
+        <MediaUpload onChange={handleUpload}>
           <IconButton className="bg-stone-900">
             <MdUpload className="h-20 w-20 m-8 text-neutral-400" />
           </IconButton>
         </MediaUpload>
         <div className="flex flex-col items-center">
-          <p>Select files to upload.</p>
+          <p>Select files to upload</p>
           <p className="text-neutral-400 text-sm">
             Your videos will be private until you publish them.
           </p>
         </div>
-        <MediaUpload
-          onChange={() => {
-            handleUpload;
-          }}
-        >
+        <MediaUpload onChange={handleUpload}>
           <Button type="box">Select Files</Button>
         </MediaUpload>
       </div>
