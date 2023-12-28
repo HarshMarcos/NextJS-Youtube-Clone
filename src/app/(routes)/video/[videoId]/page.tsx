b099637d62701f106d547be98d5736eea2455af2
admin <harshkurulkar7@gmail.com>
2023-12-28 18:50:13 +0530

    Fix Bug

--- src/app/(routes)/video/[videoId]/page.tsx
+++ src/app/(routes)/video/[videoId]/page.tsx
@@ -1,7 +1,16 @@
+import increaseVideoViewCount from "@/actions/increaseVideoViewCount";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
-};
+interface VideoPageParams {
+  videoId?: string;
+}
 
-export default page;
+export default async function VideoPage({
+  params,
+}: {
+  params: VideoPageParams;
+}) {
+  const { videoId } = params;
+  const video = await increaseVideoViewCount({ videoId });
+  return <>{video ? <div>{videoId}</div> : <h1>Video not found :(</h1>}</>;
+}
