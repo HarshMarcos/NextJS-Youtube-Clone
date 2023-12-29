ae6ba4a49dc7f2b5e3bccf1c4eae91ec93730df6
admin <harshkurulkar7@gmail.com>
2023-12-29 14:24:42 +0530

    Add deployment changes

--- src/app/(routes)/video/[videoId]/page.tsx
+++ src/app/(routes)/video/[videoId]/page.tsx
@@ -1,11 +1,12 @@
 import getChannelById from "@/actions/getChannelById";
 import getCommentsByVideoId from "@/actions/getCommentsByVideoId";
+import { getRecommendedVideos } from "@/actions/getRecommendedVideos";
 import increaseVideoViewCount from "@/actions/increaseVideoViewCount";
+import VideoCard from "@/components/shared/VideoCard";
 import CommentSection from "@/components/video/CommentSection/CommentSection";
 import Description from "@/components/video/Description";
 import LikeSubscribeSection from "@/components/video/LikeSubscribeSection/LikeSubscribeSection";
 import VideoPlayer from "@/components/video/VideoPlayer";
-import React from "react";
 
 interface VideoPageParams {
   videoId?: string;
@@ -17,29 +18,40 @@ export default async function VideoPage({
   params: VideoPageParams;
 }) {
   const { videoId } = params;
+
   const video = await increaseVideoViewCount({ videoId });
   const channel = await getChannelById({ channelId: video?.channelId });
   const comments = await getCommentsByVideoId({
     videoId,
   });
-  return (
-    <>
-      {video && channel && comments ? (
-        <div className="flex flex-col lg:flex-row mx-6 mt-2 gap-4">
-          <div className="w-full lg:w-3/4 flex flex-col gap-4">
-            <VideoPlayer videoSrc={video.videoSrc} />
-            <h1 className="text-2xl font-medium break-before-all">
-              {video.title}
-            </h1>
-            <LikeSubscribeSection video={video} channel={channel} />
-            <Description video={video} />
-            <CommentSection comments={comments} videoId={video.id} />
-            <div className="w-full lg:w-1/4 flex flex-col gap-4 pb-4"></div>
-          </div>
-        </div>
-      ) : (
-        <h1>Video not found :(</h1>
-      )}
-    </>
+  const recommendedVideos = await getRecommendedVideos({ video });
+
+  return video && channel && comments ? (
+    <div className="flex flex-col lg:flex-row mx-6 mt-2 gap-4">
+      <div className="w-full lg:w-3/4 flex flex-col gap-4">
+        <VideoPlayer videoSrc={video.videoSrc} />
+        <h1 className="text-2xl font-medium break-all">{video.title}</h1>
+        <LikeSubscribeSection video={video} channel={channel} />
+        <Description video={video} />
+        <CommentSection comments={comments} videoId={video.id} />
+      </div>
+      <div className="w-full lg:w-1/4 flex flex-col gap-4 pb-4">
+        {recommendedVideos
+          ? recommendedVideos.map((recommendedVideo) => {
+              return (
+                <VideoCard
+                  key={recommendedVideo.id}
+                  isVertical={false}
+                  video={recommendedVideo}
+                  channel={recommendedVideo.channel}
+                  channelAvatar
+                />
+              );
+            })
+          : null}
+      </div>
+    </div>
+  ) : (
+    <h1>Video not found</h1>
   );
 }
--- src/components/shared/Navigation/Navbar/Search.tsx
+++ src/components/shared/Navigation/Navbar/Search.tsx
@@ -1,15 +1,45 @@
 "use client";
-import React, { useState } from "react";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 import { MdOutlineSearch } from "react-icons/md";
+import queryString from "query-string";
 
 const Search = () => {
   const [text, setText] = useState("");
+
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const query = {
+      searchQuery: text,
+    };
+
+    const url = queryString.stringifyUrl(
+      {
+        url: "/search",
+        query,
+      },
+      {
+        skipNull: true,
+      }
+    );
+
+    router.push(url);
+  };
+
   return (
-    <form className="flex flex-row border-[1px] border-neutral-700 rounded-full overflow-hidden w-2/5">
+    <form
+      className="flex flex-row border-[1px] border-neutral-700 rounded-full overflow-hidden w-2/5"
+      onSubmit={handleSearch}
+    >
       <input
         type="text"
         placeholder="Search"
-        className="w-full px-3 py-2 bg-neutral-900"
+        className="w-full px-4 py-2 bg-neutral-900"
         onChange={(e) => setText(e.target.value)}
         value={text}
       />
